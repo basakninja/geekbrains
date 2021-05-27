19acadaf9d81cc18b350dffb780bdd1458c400b4
Uladzimir Basak <uladzimir.basak@softeq.com>
2021-05-27 14:24:43 +0300

    some changes

--- lesson-2/4. Receipt/Program.cs
+++ lesson-2/4. Receipt/Program.cs
@@ -13,6 +13,11 @@ namespace _4._Receipt
             decimal costBePVM = 2.88M;
             decimal costPVM = Math.Round(costBePVM*(storeAPersentage/100));
             decimal costSuPVM = costPVM + costBePVM;
+            string saller = "Kas ninkas: Emilija K.";
+            string kassa = "Tr:0072 Kasa:0001";
+            int lsNumber = 00000010899;
+            int kvitas = 236;
+            string address = "Seskines g. 30, Vilnius";
 
 
             decimal moketi = 3.49M;
@@ -21,22 +26,22 @@ namespace _4._Receipt
 
             Console.WriteLine($"{storeName}" +
                 $"\nProduotuve \"Drogas\",  tel.:{storePhone}" +
-                $"\nAdresas:    Seskines g. 30, Vilnius" +
-                $"\nKasa Nr.1, Sas.Nr  LS  00000010899A" +
+                $"\nAdresas:    {address}" +
+                $"\nKasa Nr.1, Sas.Nr  LS  {lsNumber}A" +
                 $"\n                          #00337738" +
-                $"\nKvitas 70/2369" +
-                $"\n   Tr:0072 Kasa:0001              #" +
-                $"\n   Kas ninkas: Emilija K.         #" +
+                $"\nKvitas 70/{kvitas}" +
+                $"\n   {kassa}              #" +
+                $"\n   {saller}         #" +
                 $"\n                                  #" +
-                $"\nLACTACYD INT.HIG.EM          3,49 A" +
+                $"\nLACTACYD INT.HIG.EM          {costSuPVM} A" +
                 $"\n+++++++++++++++++++++++++++++++++++" +
-                $"\nTarp ne suma                 3,49  " +
+                $"\nTarp ne suma                 {costSuPVM}  " +
                 $"\n+++++++++++++++++++++++++++++++++++" +
                 $"\n                              {moketi} " +
                 $"\n                                   " +
-                $"\nMoketi                       €{moketi} " +
-                $"\nMoketa                      €{moketa} " +
-                $"\nGraza                       €{moketa-moketi} " +
+                $"\nMoketi                    EUR {moketi} " +
+                $"\nMoketa                   EUR {moketa} " +
+                $"\nGraza                    EUR {moketa-moketi} " +
                 $"\nKasininkas (-) Emilija K." +
                 $"\nLF 00000010899A {date:yyyy-MM-dd HH:mm:ss}");
 
